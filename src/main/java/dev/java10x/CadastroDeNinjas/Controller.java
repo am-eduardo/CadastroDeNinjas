de59c4580a574dd0eab35a9822f31c609e8d375b
Eduardo <eduardomaiajf@gmail.com>
2025-07-23 20:14:57 -0300

    arrumando a casa

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeira menssagem nessa rota";
+    }
+
+
+
+}
